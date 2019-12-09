bf0d0fa2c2c257e778bcc42c4771c8b9d8e182f7
Zach Weber <weberzt@gmail.com>
2019-12-09 19:55:08 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -11,17 +11,3 @@ def square_array(array)
 
   return new_array
 end 
-  
-def square_array(array)
-  counter = 0 
-  
-  new_array = []
-  
-  while array[counter] do
-   puts array[counter]
-    counter += 1 
-  end
-  
-
-  return new_array
-end 
