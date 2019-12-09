42b6ec26767d20bc52a1e05cf3002039cb1740e7
Zach Weber <weberzt@gmail.com>
2019-12-09 19:57:50 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,14 +1,9 @@
-def square_array(array)
-  counter = 0 
-  
+def square_array(numbers)
   new_array = []
-  
-  while array[counter] do
-   puts array[counter]
-    counter += 1 
-  end
-  
-  
-
+  counter = 0 
+  while counter < numbers.length()
+  new_array.push(numbers[counter] * numbers[counter])
+  counter += 1
+  end 
   return new_array
-end 
+end
